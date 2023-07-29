52e05d0d16ce0521d17875089505c57da6a4e23b
Veronika Bendova <svechovaver@gmail.com>
2023-07-29 19:15:58 +0200

    Task done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('funguju!');
+const minutka = document.querySelector('.alarm');
+const zvuk = document.querySelector('audio');
+const stopTlacitko = document.querySelector('button');
+const vstupOdUzivatele = Number(
+  prompt('Zadej za jak dlouho má zazvonit minutka:'),
+);
+const zvoneni = () => {
+  minutka.classList.add('alarm--ring');
+  zvuk.play();
+};
+
+const casovac = setTimeout(zvoneni, vstupOdUzivatele * 1000);
+stopTlacitko.addEventListener('click', () => {
+  clearTimeout(casovac);
+});
